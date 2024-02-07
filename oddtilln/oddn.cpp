a840af03f4dbd71dcbc25e0b93486035b315f4c1
YASMIN <yasmin0sekh0@gmail.com>
2024-02-07 19:14:09 +0530

    print all odd numbers till n

---
+++ oddtilln/oddn.cpp
@@ -0,0 +1,18 @@
+//program to print all odd numbers till n.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        if(i%2!=0){
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+
+}
